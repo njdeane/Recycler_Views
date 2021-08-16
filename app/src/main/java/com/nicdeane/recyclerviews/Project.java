1a79557c19e5e5e56ad53c7b51317a9cfff91792
njdeane <njdeane7@gmail.com>
2021-08-16 14:49:16 +1000

    Create Project.java class and constructor for the card view it will handle.

---
+++ app/src/main/java/com/nicdeane/recyclerviews/Project.java
@@ -0,0 +1,15 @@
+package com.nicdeane.recyclerviews;
+
+public class Project {
+
+    String name;
+    String description;
+    int image;
+
+    public Project(String name, String description, int image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+
+}
